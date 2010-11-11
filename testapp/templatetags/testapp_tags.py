133fbaa9c872a8898be753fb349014ebc6c16086
Enrico <rico.bl@gmail.com>
2010-11-10 22:00:04 -0200

    Added some kwargs tests.

--- testapp/templatetags/testapp_tags.py
+++ testapp/templatetags/testapp_tags.py
@@ -12,5 +12,8 @@ def testtag(*args, **kwargs):
     output = ''
     if args:
         output += 'Args: %s'  % ', '.join(args)
+    if kwargs:
+        output += 'Kwargs: '
+        output += ', '.join(['%s=%s' % (key, value) for key, value in kwargs.iteritems()])
     return output
 
