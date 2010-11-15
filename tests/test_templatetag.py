de81d7585e669c3bf78add421d51a1f85b610a45
Enrico <rico.bl@gmail.com>
2010-11-15 21:46:34 -0200

    Converted tests to generative format.

--- tests/test_templatetag.py
+++ tests/test_templatetag.py
@@ -4,50 +4,49 @@ from nose.tools import assert_equals
 
 from django.template import Template, Context
 
-def assertRenderEquals(tag_code, expected, context=None):
+def assert_render_equals(tag_code, expected, context=None):
     t = Template("{% load dummyapp_tags %}" + tag_code)
     content = t.render(Context(context or {}))
     assert_equals(content, expected)
 
 def test_tag_with_no_args_prints_nothing():
-    assertRenderEquals("{% sampletag %}", '')
+    yield assert_render_equals, "{% sampletag %}", ''
 
 def test_tag_with_single_arg_prints_arg():
-    assertRenderEquals("{% sampletag 'yo' %}", "Args: yo")
+    yield assert_render_equals, "{% sampletag 'yo' %}", "Args: yo"
 
 def test_tag_with_multiple_args_prints_arg_list():
-    assertRenderEquals("{% sampletag 'yo','hey' %}", "Args: yo, hey")
+    yield assert_render_equals, "{% sampletag 'yo','hey' %}", "Args: yo, hey"
     # Vary spacing around comma
-    assertRenderEquals("{% sampletag 'yo', 'hey' %}", "Args: yo, hey")
-    assertRenderEquals("{% sampletag 'yo' ,'hey' %}", "Args: yo, hey")
-    assertRenderEquals("{% sampletag 'yo' , 'hey' %}", "Args: yo, hey")
+    yield assert_render_equals, "{% sampletag 'yo', 'hey' %}", "Args: yo, hey"
+    yield assert_render_equals, "{% sampletag 'yo' ,'hey' %}", "Args: yo, hey"
+    yield assert_render_equals, "{% sampletag 'yo' , 'hey' %}", "Args: yo, hey"
 
 def test_tag_with_single_kwarg_prints_keyword_and_value():
-    assertRenderEquals("{% sampletag kw1='yo' %}", "Kwargs: kw1=yo")
+    yield assert_render_equals, "{% sampletag kw1='yo' %}", "Kwargs: kw1=yo"
 
 def test_tag_with_multiple_kwargs_prints_a_keyword_and_value_list():
-    assertRenderEquals("{% sampletag kw1='yo',kw2='hey' %}", "Kwargs: kw1=yo, kw2=hey")
+    yield assert_render_equals, "{% sampletag kw1='yo',kw2='hey' %}", "Kwargs: kw1=yo, kw2=hey"
     # Vary spacing around comma
-    assertRenderEquals("{% sampletag kw1='yo', kw2='hey' %}", "Kwargs: kw1=yo, kw2=hey")
-    assertRenderEquals("{% sampletag kw1='yo' ,kw2='hey' %}", "Kwargs: kw1=yo, kw2=hey")
-    assertRenderEquals("{% sampletag kw1='yo' , kw2='hey' %}", "Kwargs: kw1=yo, kw2=hey")
+    yield assert_render_equals, "{% sampletag kw1='yo', kw2='hey' %}", "Kwargs: kw1=yo, kw2=hey"
+    yield assert_render_equals, "{% sampletag kw1='yo' ,kw2='hey' %}", "Kwargs: kw1=yo, kw2=hey"
+    yield assert_render_equals, "{% sampletag kw1='yo' , kw2='hey' %}", "Kwargs: kw1=yo, kw2=hey"
 
 def test_tag_with_context_prints_keyword_and_value_list():
-    assertRenderEquals("{% sampletag_withcontext %}",
-                            "Context: myvar=yo", context={'myvar': 'yo'})
+    yield assert_render_equals, "{% sampletag_withcontext %}", "Context: myvar=yo", {'myvar': 'yo'}
 
 def test_tag_prints_filtered_arg():
-    assertRenderEquals("{% sampletag 'yo'|upper %}", "Args: YO")
+    yield assert_render_equals, "{% sampletag 'yo'|upper %}", "Args: YO"
 
 def test_tag_prints_filtered_kwarg():
-    assertRenderEquals("{% sampletag kw1='yo'|upper %}", "Kwargs: kw1=YO")
+    yield assert_render_equals, "{% sampletag kw1='yo'|upper %}", "Kwargs: kw1=YO"
 
 def test_tag_prints_variable_arg():
-    assertRenderEquals("{% sampletag myvar %}", "Args: yo", context={'myvar': 'yo'})
+    yield assert_render_equals, "{% sampletag myvar %}", "Args: yo", {'myvar': 'yo'}
 
 def test_tag_prints_variable_kwarg():
-    assertRenderEquals("{% sampletag kw1=myvar %}", "Kwargs: kw1=yo", context={'myvar': 'yo'})
+    yield assert_render_equals, "{% sampletag kw1=myvar %}", "Kwargs: kw1=yo", {'myvar': 'yo'}
 
 def test_tag_with_space_in_arg():
-    assertRenderEquals("{% sampletag 'yo hey' %}", "Args: yo hey")
+    yield assert_render_equals, "{% sampletag 'yo hey' %}", "Args: yo hey"
 
